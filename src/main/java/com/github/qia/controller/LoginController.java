4fdb081924efa90c6fb1f6e09a4d35be7af4f833
ztxtxwd <ztx4215@gmail.com>
2019-03-30 19:22:45 +0800

    添加了实验室板块

--- src/main/java/com/github/qia/controller/LoginController.java
+++ src/main/java/com/github/qia/controller/LoginController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import com.alibaba.fastjson.JSON;
 import com.github.qia.pojo.User;
 import com.github.qia.service.UserService;
+import com.github.qia.util.CookieUtil;
 
 import entity.Result;
 
@@ -24,6 +27,12 @@ public class LoginController {
 
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private HttpServletResponse response;
 	
 	@RequestMapping("/signin")
 	public Result signin(@RequestBody User user){
@@ -35,13 +44,28 @@ public class LoginController {
 		}
 		User userByName=userService.findOne(user.getUsername());
 		if (BCrypt.checkpw(user.getPassword(),userByName.getPassword())) {
-			HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+			/*HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
 			request.setAttribute("userId", userByName.getId());
-			request.setAttribute("userName", userByName.getUsername());
-
+			request.setAttribute("userName", userByName.getUsername());*/
+			CookieUtil.setCookie(request, response, "username", user.getUsername(), 3600*24, "UTF-8");
+			//CookieUtil.setCookie(request, response, "pwd", user.getPassword(), 3600*24, "UTF-8");
 			return new Result(true, userByName.getId()+"");
 		}else{
 			return new Result(false, "登录失败");
+		}
+	}
+	
+	@RequestMapping("/signout")
+	public Result signout(){
+		try {
+			CookieUtil.deleteCookie(request, response, "username");
+			//CookieUtil.deleteCookie(request, response, "pwd");
+			return new Result(true, "注销成功");
+			  
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return new Result(false, "注销失败");
 		}
 	}
 }
